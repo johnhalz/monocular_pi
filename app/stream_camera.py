4cdf3d711bbf7da0dea93e096b935cbf3ee835b3
John <john.halazonetis@icloud.com>
2023-01-26 19:25:35 +0100

    Init commit

---
+++ app/stream_camera.py
@@ -0,0 +1,35 @@
+# Import the necessary packages
+import time
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+# def main():
+#     # Initialize the camera and grab a reference to the raw camera capture
+#     camera = PiCamera()
+#     camera.resolution = (640, 480)
+#     camera.framerate = 32
+#     rawCapture = PiRGBArray(camera, size=(640, 480))
+
+#     # Allow the camera to warmup
+#     time.sleep(0.1)
+
+#     # Capture frames from the camera
+#     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+#         # Grab the raw NumPy array representing the image, then initialize the timestamp and occupied/unoccupied text
+#         image = frame.array
+#         print('Got frame')
+        
+#         # show the frame
+#         # cv2.imshow("Frame", image)
+#         key = cv2.waitKey(1) & 0xFF
+        
+#         # clear the stream in preparation for the next frame
+#         rawCapture.truncate(0)
+
+#         # if the `q` key was pressed, break from the loop
+#         if key == ord("q"):
+#             break
+
+# if __name__ == '__main__':
+#     main()
---
+++ setup.sh
@@ -0,0 +1,1 @@
+sudo apt install -y ffmpeg libsm6 libxext6
