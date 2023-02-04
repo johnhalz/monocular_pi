906c22816e44103660bb580fbcacd0d36779b559
John Halazonetis <john.halazonetis@icloud.com>
2023-02-04 21:41:39 +0100

    Improved stream scripts

--- app/stream_all.py
+++ app/stream_all.py
@@ -3,7 +3,7 @@ from sys import path
 from pathlib import Path
 import logging
 
-path.append(Path(__file__).parents[2].as_posix())
+path.append(Path(__file__).parents[1].as_posix())
 
 from src import Camera, IMU
 
@@ -26,6 +26,9 @@ def main():
 
     camera.stop_stream()
     imu.stop_stream()
+
+    camera.disconnect()
+    imu.disconnect()
 
 if __name__ == '__main__':
     main()
