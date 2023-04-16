6fa85e8473ecbdca79aaabccaa5f6f0b8eebe2c3
John Halazonetis <john.halazonetis@icloud.com>
2023-04-16 22:27:15 +0200

    Added simulation argument to sensor class

--- src/io_element/sensor.py
+++ src/io_element/sensor.py
@@ -1,5 +1,6 @@
 '''Sensor class module'''
 import logging
+from pathlib import Path
 from typing import Union, Dict
 
 from .io_element import IOElement, Message
@@ -8,11 +9,21 @@ class Sensor(IOElement):
     '''
     Sensor class (child class of IOElement)
     '''
-    def __init__(self, name: str, topic: str) -> None:
+    def __init__(self, name: str, topic: str, simulated: Path = None) -> None:
         super().__init__(name, topic)
         self.connection = None
         self.read_source = None
         self.connected: bool = False
+        self.simulated = False
+        if simulated is not None:
+            self.simulated = True
+
+            if simulated.exists():
+                self.simulation_file = simulated
+            else:
+                raise FileNotFoundError(
+                    f'{self.name}: Input ismulation file {simulated.as_posix()} was not found.'
+                )
 
     def connect(self) -> None:
         '''Method to connect to sensor.'''
