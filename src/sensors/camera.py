f3e6322d5bb57ff9be03192420045f74d0843e3a
John Halazonetis <john.halazonetis@icloud.com>
2023-04-16 22:27:30 +0200

    Added simulation to camera class

--- src/sensors/camera.py
+++ src/sensors/camera.py
@@ -1,6 +1,7 @@
 '''Camera sensor module'''
 import logging
 from typing import Dict
+from time import sleep
 
 import cv2
 import numpy as np
@@ -8,16 +9,20 @@ from io_element import Sensor, Message
 from messages import CompressedImage
 
 # pylint: disable=too-few-public-methods
+# pylint: disable=attribute-defined-outside-init
+# pylint: disable=no-member
 class Camera(Sensor):
     '''Camera sensor class'''
-    def __init__(self, name: str, topic: str = 'camera') -> None:
-        super().__init__(name, topic)
-
     def connect(self) -> None:
+        '''Connect to camera'''
         super().connect()
         try:
-            self.connection = cv2.VideoCapture(0)
-            self.read_source = self.connection
+            if not self.simulated:
+                self.connection = cv2.VideoCapture(0)
+            else:
+                self.connection = cv2.VideoCapture(self.simulation_file.as_posix())
+                self.sim_fps = self.connection.get(cv2.CAP_PROP_FPS)
+
             self.connected = True
             logging.info(f'{self.name}: Successfully connected.')
         except Exception as exc:
@@ -31,8 +36,12 @@ class Camera(Sensor):
         -------
         -`Message`: CompressedImage (protobuf message)
         '''
+        # If simulated- Simulate framerate of input video
+        if self.simulated:
+            sleep(1./self.sim_fps)
+
         self.timestamp.GetCurrentTime()
-        success, image = self.read_source.read()
+        success, image = self.connection.read()
         if not success:
             return self.timestamp, None
 
